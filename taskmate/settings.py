4769efac9f659fbd93d2fddcc163759a813528c7
Zarmin <raimovazarminakhon@gmail.com>
2024-07-12 17:16:34 +0600

    Railway Updates

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -29,7 +29,7 @@ DEBUG = env.bool("DJANGO_DEBUG")
 
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
